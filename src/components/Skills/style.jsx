09ebbbeb0578620fbab3ae25c8a422ab3149ab38
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-25 15:07:46 +0200

    start skills comp

---
+++ src/components/Skills/style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.section`
+  color: white;
+  h1 {
+    color: #f2b84b;
+    font-size: 2em;
+    font-weight: bold;
+    letter-spacing: 0.2em;
+  }
+  p {
+    color: #6a6a6a;
+    font-size: 2em;
+  }
+  .articleContainer {
+    display: flex;
+  }
+`;
