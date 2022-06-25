a4a82a0b5e3fe35fea1adf7b7db09bdffa31b82c
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-25 17:18:10 +0200

    fixing style

--- src/components/Intro/style.jsx
+++ src/components/Intro/style.jsx
@@ -4,7 +4,6 @@ export default styled.section`
   color: white;
   background-color: #e9e9e929;
   width: 22vw;
-  height: 96vh;
   border: 1px gray solid;
   border-radius: 8px;
   display: flex;
@@ -49,7 +48,7 @@ export default styled.section`
 
   .swiper {
     margin-top: 4vh;
-    width: 168px;
+    width: 8vw;
     filter: drop-shadow(0px 3px 4px rgba(243, 243, 243, 0.664));
   }
   .swiper-slide {
@@ -57,7 +56,7 @@ export default styled.section`
     justify-content: center;
     border-radius: 50%;
     img {
-      width: 168px;
+      width: 8vw;
     }
   }
 `;
--- src/pages/style.jsx
+++ src/pages/style.jsx
@@ -6,5 +6,4 @@ export default styled.div`
   min-height: 100vh;
   background-color: #1b252d;
   display: flex;
-  align-items: center;
 `;
