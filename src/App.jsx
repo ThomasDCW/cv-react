0804fde0b21dc96ea877ad3781aabc859c353180
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-26 09:26:11 +0200

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import VariableCSS from './AppStyle.jsx';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
+      <VariableCSS>
         <HomePage />
-      </header>
+      </VariableCSS>
     </div>
   );
 }
