8fa58b7399605619de7782476489d439e1207b51
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-26 18:03:07 +0200

    del animation

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -17,40 +17,47 @@ export default styled.section`
     font-size: 1.2em;
     font-weight: bold;
   }
-  a {
-    background: linear-gradient(to top, #f2b84b, #f2b84b, transparent 5%)
-      no-repeat left bottom / 0 100%;
-    transition: background-size 0.6s;
-  }
-  a:hover {
-    background-size: 100% 100%;
-  }
+
   .articleContainer {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     letter-spacing: 0.2em;
     margin-top: 4vh;
+    a {
+      text-decoration: none;
+      background: linear-gradient(to top, #f2b84b, #f2b84b, transparent 5%)
+        no-repeat left bottom / 0 100%;
+      transition: background-size 1s;
+    }
+    a:visited {
+      color: var(--text);
+      text-decoration: none;
+    }
+    a:hover {
+      background-size: 100% 100%;
+    }
     img {
       width: 48px;
       height: 48px;
       margin-right: 1.6em;
-      animation: rotation 6s linear infinite;
-    }
-    li {
-      display: flex;
-      align-items: center;
-      font-size: 1.2em;
-      margin-bottom: 1.2em;
-    }
-    .firstArticle {
-      li {
-        margin-right: 10vw;
-      }
-
-      border-right: 2px solid var(--transparantGrey);
     }
   }
+
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 1.2em;
+    margin-bottom: 1.2em;
+  }
+  .firstArticle {
+    li {
+      margin-right: 10vw;
+    }
+
+    border-right: 2px solid var(--transparantGrey);
+  }
+
   .border {
     margin-left: 10vw;
     margin-right: 10vw;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Reset } from 'styled-reset';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Reset />
-    <App />
+    <BrowserRouter>
+      <Reset />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
