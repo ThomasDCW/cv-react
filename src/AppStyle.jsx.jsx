359e92248863d3401fd93afb869875d1c45c6c9c
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-26 09:25:14 +0200

    create variable

---
+++ src/AppStyle.jsx.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=Poppins&display=swap');
+  //Variables:
+  --primary: #f2b84b;
+  --secondary: #6a6a6a;
+  --background: #1b252d;
+  --transparantGrey: #e9e9e929;
+  --text: white;
+`;
